9af5c12fd7095d910deae5a98ad94719fd2a6cf3
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-11-09 20:48:11 +0700

    feat: belajar Math

---
+++ MathExample.cs
@@ -0,0 +1,21 @@
+using System;
+
+class MathExample
+{
+  public static void Max()
+  {
+    Console.WriteLine(Math.Max(5, 10));
+    // ini akan return nilai terbesar
+  }
+
+  public static void Min()
+  {
+    Console.WriteLine(Math.Min(5,10));
+    // kebalikan dari max. bakal return nilai terkecil lah
+  }
+
+  public static void Sqrt()
+  {
+    Console.WriteLine(Math.Sqrt(64));
+  }
+}
